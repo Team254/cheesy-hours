60a4a2e91ae82692c2933307b1c84ed84a289620
Ryan Johnson <ryan@johnsonweb.us>
2016-01-10 19:44:18 -0800

    All of the RFID Code 2

---
+++ db/migrations/006_create_tag_table.rb
@@ -0,0 +1,10 @@
+Sequel.migration do
+  change do
+    create_table(:tags) do
+      primary_key :id
+      String :tag_id, :null => false
+      Integer :student_id, :null => true
+      Integer :mentor_id, :null => true
+    end
+  end
+end
