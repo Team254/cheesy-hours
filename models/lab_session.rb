cbba2fde2bec757659bf4499828b2c59e6ec045d
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2018-02-10 14:36:47 -0800

    temp change

--- models/lab_session.rb
+++ models/lab_session.rb
@@ -8,6 +8,7 @@ class LabSession < Sequel::Model
   many_to_one :mentor
 
   def duration_hours
-    time_out.nil? ? 0 : (time_out - time_in) / 3600
+    0
+    # time_out.nil? ? 0 : (time_out - time_in) / 3600
   end
 end
